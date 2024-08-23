9be7e0b8ad9cb40e48946156fed7f8818954f6a7
Vishal Jaiswar <vishaldevloperfd@gmail.com>
2024-08-23 17:01:31 +0530

    updates

--- basic-app/src/App.js
+++ basic-app/src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Hello from './components/Hello';
 import CC1 from './components/CC1';
+import One from './components/One';
+
 
 function App() {
   return (
     <div className="App">
       {/* <Hello /> */}
-      
+      <One />
       <CC1/>
       {/* <h1 style={{color:"red", background:"lightblue"}}> CSS in ReactJS</h1> */}
     </div>
--- basic-app/src/components/CC1.js
+++ basic-app/src/components/CC1.js
@@ -1,18 +1,33 @@
 import React, {Component} from "react";
 import '../components/CC1.css';
 
+
 class CC1 extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            color: "yellow",
+            bgcolor: "black"
+        }
+    }
+    changecolor = () =>{
+        this.setState({color:'green'})
+    }
+    changebgcolor = () =>{
+        this.setState({bgcolor:'blue'})
+    }
+
 
     render(){
         return(
             <div>
                 <h2 className="hello">External CSS in ReactJS</h2>
-                <button>Change Color</button><br/><br/>
-                <button>Change BG-Color</button>
+                <button onClick={this.changecolor}>Change Color</button><br/><br/>
+                <button onClick={this.chan}>Change BG-Color</button>
                 {/* <h1 style={{color:"red", background:"lightblue"}}> CSS in ReactJS</h1> */}
             </div>
         )
     }
 }
 
-export default CC1
+export default CC1;
